15c0a6a52c1cd816b5103315de17b4950678c0ca
avillegas40415 <avillegas40415@ufide.ac.cr>
2024-11-06 18:10:19 -0600

    Proyecto base
    Se publica el proyecto base e inicial del sitio web de EVO Gym

---
+++ ProyectoEVO/src/main/java/com/EVO/gym/EvoGymApplication.java
@@ -0,0 +1,13 @@
+package com.EVO.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoGymApplication.class, args);
+	}
+
+}
