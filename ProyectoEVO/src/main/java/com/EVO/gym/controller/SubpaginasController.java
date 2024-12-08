d071a88ea4ff4ac21996484d0f64396c3d74f082
avillegas40415 <avillegas40415@ufide.ac.cr>
2024-12-08 15:40:11 -0600

    Revert "Nuevas vistas publicas"
    This reverts commit 3e2844371deb01ee24671aed0627324543413fd1.

--- ProyectoEVO/src/main/java/com/EVO/gym/controller/SubpaginasController.java
+++ ProyectoEVO/src/main/java/com/EVO/gym/controller/SubpaginasController.java
@@ -1,18 +1,10 @@
 package com.EVO.gym.controller;
 
-import com.EVO.gym.domain.Membresia;
-import com.EVO.gym.service.MembresiaService;
-import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class SubpaginasController {
-    
-    @Autowired
-    private MembresiaService membresiaService;
     
     @GetMapping("/index")
     public String index() {
@@ -22,33 +14,6 @@ public class SubpaginasController {
     @GetMapping("/nosotros")
     public String nosotros() {
         return "nosotros"; // Retorna el nombre de la vista (nosotros.html)
-    }
-    
-    @GetMapping("/gimnasio")
-    public String gimnasio() {
-        return "gimnasio"; // Retorna el nombre de la vista (nosotros.html)
-    }
-    
-    /*
-    @GetMapping("/planes")
-    public String planes() {
-        return "planes"; // Retorna el nombre de la vista (nosotros.html)
-    }*/
-    @GetMapping("/planes")
-    public String planes(Model model) {
-        List<Membresia> membresias = membresiaService.getMembresias(true); // Obtener las membresías
-        model.addAttribute("membresias", membresias); // Agregarlas al modelo
-        return "planes"; // Retornar el nombre de la vista
-    }
-    
-    @GetMapping("/productos")
-    public String productos() {
-        return "productos"; // Retorna el nombre de la vista (nosotros.html)
-    }
-    
-    @GetMapping("/contacto")
-    public String contacto() {
-        return "contacto"; // Retorna el nombre de la vista (nosotros.html)
     }
     
     @GetMapping("/login")
