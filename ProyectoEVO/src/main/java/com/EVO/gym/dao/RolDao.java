59cc9171a1e43f94dca383c12b0a961fbb359585
JoseSolano0810 <jsolano10816@ufide.ac.cr>
2024-12-17 21:21:52 -0600

    Usuarios - Roles - Correo
    commit de usurio, roles y correo

---
+++ ProyectoEVO/src/main/java/com/EVO/gym/dao/RolDao.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.EVO.gym.dao;
+import com.EVO.gym.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
